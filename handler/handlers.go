20fddf623bc662267b7ac84e2a91cdbffb94a10a
solarcreature <kothasanju@gmail.com>
2022-06-10 15:52:11 -0600

    Redirection and Forwarding

---
+++ handler/handlers.go
@@ -0,0 +1,39 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/solarcreature/GoURL/algo"
+	"github.com/solarcreature/GoURL/store"
+	"net/http"
+)
+
+type URLRequest struct {
+	LongURL string `json:"long_url" binding:"required"`
+	UserID string `json:"user_id" binding:"required"`
+}
+func CreateShortURL(c *gin.Context) {
+	var request URLRequest
+	err := c.ShouldBindJSON(&request) 
+	if err != nil {
+		c.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
+		
+		return
+	}
+
+	shortURL := algo.GenerateURL(request.LongURL,request.UserID)
+	store.StoreMap(shortURL,request.LongURL,request.UserID)
+
+	host := "http://localhost:9808/"
+
+	c.JSON(200,gin.H{
+		"message":  	"URL shortened successfully",
+		"short_url":	 host + shortURL,
+	})
+}
+
+func RedirectShortURL(c *gin.Context) {
+	shortURL := c.Param("shortURL")
+	initialURL := store.RetrieveOriginalURL(shortURL)
+	c.Redirect(302,initialURL)
+
+}
--- main.go
+++ main.go
@@ -3,15 +3,27 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/solarcreature/GoURL/store"
+	"github.com/solarcreature/GoURL/handler"
 )
 
 func main() {
 	r := gin.Default()
 	r.GET("/",func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Web Server works",
+			"message": "Welcome to GoURL!",
 		})
 	})
+
+	r.POST("/create-short-url", func(c *gin.Context){
+		handler.CreateShortURL(c)
+	})
+
+	r.GET("/shortURL", func(c *gin.Context){
+		handler.RedirectShortURL(c)
+	})
+
+	store.InitializeStore()
 
 	err := r.Run(":9808")
 	if err != nil {
